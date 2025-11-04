5d319e199757fc5e834f346a3b7e14a074e13f0f
suelina <suelina001@gmail.com>
2025-11-04 15:57:43 +0900

    두번째 프로젝트 시작

---
+++ my-second-app/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./component/Common/Footer/Footer"
+
+
+function App() {
+
+	return (
+		<>
+			<Footer/>
+		</>
+	)
+
+}
+
+export default App
---
+++ my-second-app/src/component/Common/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import { StyledFooter } from "./Footer.styles"
+
+const Footer = ( )=>{
+    return <StyledFooter>푸터</StyledFooter>
+}
+
+
+export default Footer;

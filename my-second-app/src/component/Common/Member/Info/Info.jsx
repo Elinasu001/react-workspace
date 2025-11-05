24559cb528a1dfd1eff3308054b5d1a2b68bc615
suelina <suelina001@gmail.com>
2025-11-05 12:02:07 +0900

    개선 : useEffect로 사용

--- my-second-app/src/component/Common/Member/Info/Info.jsx
+++ my-second-app/src/component/Common/Member/Info/Info.jsx
@@ -1,5 +1,5 @@
 import { useNavigate }   from "react-router-dom";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../../context/AuthContext";
 
 // 사용자가 url을 통해 접근하는 것을 방지하기 위해서
@@ -11,15 +11,16 @@ const Info = () => {
     const { auth } = useContext(AuthContext);
     const navi = useNavigate();
 
-
-    if(!auth.authenticated){
-        alert("로그인이 필요한 페이지입니다.");
-        navi("/login");
-    }
+    useEffect(() => {
+        if(!auth.authenticated){
+            alert("로그인이 필요한 페이지입니다.");
+            navi("/login");
+        }
+    }, [auth, navi]);
 
     return (
         <>
-            
+
 
         </>
     )
